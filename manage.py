c2517b4150ea761964f9777124dadacd5236bfb7
Mihai Bivol <mm.bivol@gmail.com>
2015-01-19 17:29:44 +0200

    Add default logger format

--- manage.py
+++ manage.py
@@ -7,7 +7,11 @@ app = create_app()
 
 
 def main():
-    logging.basicConfig(loglevel=logging.DEBUG)
+    default_format = '%(asctime)s - %(name)s - %(levelname)s: %(message)s'
+    default_datefmt = '%d-%m-%Y %H:%M'
+    logging.basicConfig(loglevel=logging.DEBUG,
+                        format=default_format,
+                        datefmt=default_datefmt)
     logging.getLogger('werkzeug').setLevel(logging.INFO)
     manager = create_manager(app)
     manager.run()
