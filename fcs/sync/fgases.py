14923a85902aff9f2ffdf6ef4118e9779874ef1e
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2015-01-20 15:06:09 +0200

    [fgases] update users based on email instead of username

--- fcs/sync/fgases.py
+++ fcs/sync/fgases.py
@@ -159,7 +159,7 @@ def parse_undertaking(data):
     for contact_person in contact_persons:
         user = (
             User.query
-            .filter_by(username=contact_person['username'])
+            .filter_by(email=contact_person['email'])
             .first()
         )
         # add or update
@@ -170,6 +170,7 @@ def parse_undertaking(data):
             db.session.add(user)
         if user not in existing_persons:
             undertaking.contact_persons.append(user)
+
     current_emails = [p.get('email') for p in contact_persons]
     for person in undertaking.contact_persons:
         if person.email not in current_emails:
