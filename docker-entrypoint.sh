508efeb7dfde2f5c846db8e2d34294eed1d45595
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2016-08-25 10:37:22 +0300

    Update manage.py allowed commands

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set -e
 
-COMMANDS="add_verified_flag_mp alembic countries_un create_superuser create_user import meetings migrate_hint remove_missing_countries rq runserver shell update_representing"
+COMMANDS="shell utils db sync runserver api match"
 
 if [ -z "$MYSQL_ADDR" ]; then
     MYSQL_ADDR="mysql"
 fi
 
 while ! nc -z $MYSQL_ADDR 3306; do
-  echo "Waiting for MySQL server at '$MYSQL_ADDR' to accept connections..."
+  echo "Waiting for MySQL server at '$MYSQL_ADDR' to accept connections on port 3306..."
   sleep 3s
 done
 
 if [ ! -e .skip-db-init ]; then
   touch .skip-db-init
-  echo "Running DB CMD: ./manage.py alembic upgrade head"
-  python manage.py alembic upgrade head
+  echo "Running DB CMD: ./manage.py db init"
+  python manage.py db init
 fi
 
 if [ -z "$1" ]; then
+  echo "Serving on port 5000"
   exec waitress-serve --port 5000 manage:app
 fi
 
