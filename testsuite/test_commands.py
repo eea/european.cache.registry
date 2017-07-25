5ad5d1b9298f3dfa1172493ae6b09ac3636a0bbe
Andreea Dima <andreeadima95@gmail.com>
2017-07-25 12:09:28 +0300

    [refs #85843] Test commands

---
+++ testsuite/test_commands.py
@@ -0,0 +1,22 @@
+import json
+
+from fcs import models
+from fcs.sync.commands import update_undertakings
+from fcs.sync.fgases import eea_double_check_fgases
+from fcs.sync.ods import eea_double_check_ods
+
+
+def test_update_undertakings_fgas(client):
+    with open('testsuite/fixtures/companies-fgas.json') as file:
+        data = json.load(file)
+    undertakings_count = update_undertakings(data, eea_double_check_fgases)
+    assert undertakings_count == len(data)
+    assert models.Undertaking.query.fgases().count() == len(data)
+
+
+def test_update_undertakings_ods(client):
+    with open('testsuite/fixtures/companies-ods.json') as file:
+        data = json.load(file)
+    undertakings_count = update_undertakings(data, eea_double_check_ods)
+    assert undertakings_count == len(data)
+    assert models.Undertaking.query.ods().count() == len(data)
