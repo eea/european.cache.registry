1b36bddaadb42b7b0e2c20dee79c3420a757efe0
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-09-06 15:26:33 +0300

    [refs #87943] Add eea double checks for ODS

--- testsuite/test_commands.py
+++ testsuite/test_commands.py
@@ -17,6 +17,6 @@ def test_update_undertakings_fgas(client):
 def test_update_undertakings_ods(client):
     with open('testsuite/fixtures/companies-ods.json') as file:
         data = json.load(file)
-    undertakings_count = update_undertakings(data, eea_double_check_ods)
-    assert undertakings_count == len(data)
-    assert models.Undertaking.query.ods().count() == len(data)
+        for undertaking in data:
+            ok = eea_double_check_ods(undertaking)
+            assert ok
