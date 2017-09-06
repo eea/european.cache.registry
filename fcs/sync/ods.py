cd66beb5a87451beaf706d64fd83687b3e825075
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-09-06 15:26:33 +0300

    [refs #87943] Add eea double checks for ODS

--- fcs/sync/ods.py
+++ fcs/sync/ods.py
@@ -1,4 +1,5 @@
 from flask import current_app
+from instance.settings import  ODS_BUSINESS_PROFILES, ODS_TYPES
 
 
 def eea_double_check_ods(data):
@@ -12,21 +13,76 @@ def eea_double_check_ods(data):
     """.format(data['id'], data['status'],
                data['businessProfile']['highLevelUses'], data['types'],
                data['contactPersons'], data['domain'])
-    ok = True
 
-    if not all(('status' in data, data['status'] in ['VALID', 'DISABLED'])):
+    required_fields = ['@type', 'id', 'name', 'address', 'phone', 'domain',
+                       'contactPersons', 'dateCreated', 'dateUpdated',
+                       'status', 'eoriNumber', 'types',
+                       'businessProfile']
+    required_fields_country = ['code', 'name', 'type']
+    required_fields_user = ['userName', 'firstName', 'lastName',
+                            'emailAddress']
+    ok = True
+    for field in required_fields:
+        if not all((field in data, data[field])):
+            message = "Organisation {0} is missing.".format(field)
+            current_app.logger.warning(message + identifier)
+            ok = False
+
+    if data['@type'] != 'ODSUndertaking':
+        message = "Organisation type is not ODSUndertaking."
+        current_app.logger.warning(message + identifier)
+        ok = False
+
+    if not data['domain'] == 'ODS':
+        message = "Organisation domain is not ODS."
+        current_app.logger.warning(message + identifier)
+        ok = False
+
+    if not data['status'] in ['VALID', 'DISABLED']:
         message = 'Organisation status differs from VALID or DISABLED.'
         current_app.logger.warning(message + identifier)
         ok = False
 
     if all([data['status'] == 'DISABLED', len(data['contactPersons']) > 0]):
-        message = "Contact Persons available for DISABLED company"
+        message = "Contact Persons available for DISABLED company."
         current_app.logger.warning(message + identifier)
         ok = False
 
-    if not data['domain'] == 'ODS':
-        message = "Organisation domain is not ODS"
+    if not all(('country' in data['address'], data['address']['country'])):
+        message = 'Organisation address country is missing.'
         current_app.logger.warning(message + identifier)
         ok = False
+
+    country = data['address']['country']
+    for field in required_fields_country:
+        if not all((field in country, country[field])):
+            message = "Organisation country {0} is missing.".format(field)
+            current_app.logger.warning(message + identifier)
+            ok = False
+
+    if country['type'] != 'EU_TYPE':
+        message = "Organisation country type is not EU_TYPE."
+        current_app.logger.warning(message + identifier)
+        ok = False
+
+    for user in data['contactPersons']:
+        for field in required_fields_user:
+            if not all((field in user, user[field])):
+                message = "Organisation user {0} is missing.".format(field)
+                current_app.logger.warning(message + identifier)
+                ok = False
+    for type in data['types']:
+        if type not in ODS_TYPES:
+            message = "Organisation type {0} is not accepted.".format(type)
+            current_app.logger.warning(message + identifier)
+            ok = False
+
+    for high_level_use in data['businessProfile']['highLevelUses']:
+        if high_level_use not in ODS_BUSINESS_PROFILES:
+            message = "Organisation highlevel use {0} is not accepted.".format(
+                high_level_use
+            )
+            current_app.logger.warning(message + identifier)
+            ok = False
 
     return ok
