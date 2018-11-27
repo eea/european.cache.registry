186011ec371b4dc6aed0589461c02367b32012df
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-11-27 13:12:04 +0200

    [refs #100456] Set doublechecks logs to warning

--- cache_registry/sync/ods.py
+++ cache_registry/sync/ods.py
@@ -27,53 +27,53 @@ def eea_double_check_ods(data):
     for field in required_fields:
         if not all((field in data, data[field])):
             message = "Organisation {0} field is missing.".format(field)
-            current_app.logger.error(message + identifier)
+            current_app.logger.warning(message + identifier)
             ok = False
 
     if data['@type'] != 'ODSUndertaking':
         message = "Organisation type is not ODSUndertaking."
-        current_app.logger.error(message + identifier)
+        current_app.logger.warning(message + identifier)
         ok = False
 
     if not data['domain'] == ODS:
         message = "Organisation domain is not ODS."
-        current_app.logger.error(message + identifier)
+        current_app.logger.warning(message + identifier)
         ok = False
 
     if data['status'] not in ['VALID']:
         message = 'Organisation status differs from VALID.'
-        current_app.logger.error(message + identifier)
+        current_app.logger.warning(message + identifier)
         ok = False
 
     if not all(('country' in data['address'], data['address']['country'])):
         message = 'Organisation address country is missing.'
-        current_app.logger.error(message + identifier)
+        current_app.logger.warning(message + identifier)
         ok = False
 
     country = data['address']['country']
     for field in required_fields_country:
         if not all((field in country, country[field])):
             message = "Organisation country {0} is missing.".format(field)
-            current_app.logger.error(message + identifier)
+            current_app.logger.warning(message + identifier)
             ok = False
 
     if country['type'] != 'EU_TYPE':
         message = "Organisation country type is not EU_TYPE."
-        current_app.logger.error(message + identifier)
+        current_app.logger.warning(message + identifier)
         ok = False
 
     for user in data['contactPersons']:
         for field in required_fields_user:
             if not all((field in user, user[field])):
                 message = "Organisation user {0} is missing.".format(field)
-                current_app.logger.error(message + identifier)
+                current_app.logger.warning(message + identifier)
                 ok = False
 
     types = [object.type for object in Type.query.filter_by(domain=ODS)]
     for type in data['types']:
         if type not in types:
             message = "Organisation type {0} is not accepted.".format(type)
-            current_app.logger.error(message + identifier)
+            current_app.logger.warning(message + identifier)
             ok = False
 
     businessprofiles = [object.highleveluses for object in
@@ -84,7 +84,7 @@ def eea_double_check_ods(data):
 
     if not (high_level_uses_set or set(data['types']) & NO_HIGHLEVEL_TYPES):
         message = "Organisation high level uses are missing."
-        current_app.logger.error(message + identifier)
+        current_app.logger.warning(message + identifier)
         ok = False
 
     for high_level_use in data['businessProfile']['highLevelUses']:
@@ -92,7 +92,7 @@ def eea_double_check_ods(data):
             message = "Organisation highlevel use {0} is not accepted.".format(
                 high_level_use
             )
-            current_app.logger.error(message + identifier)
+            current_app.logger.warning(message + identifier)
             ok = False
         elif high_level_use not in NOT_OBLIGED_TO_REPORT:
             high_level_uses_set.remove(high_level_use)
@@ -102,7 +102,7 @@ def eea_double_check_ods(data):
         message = "Organisations with highlevel uses {0} should not be reported.".format(
             ', '.join(high_level_uses_set)
         )
-        current_app.logger.error(message + identifier)
+        current_app.logger.warning(message + identifier)
         ok = False
 
     return ok
