f191656df520a6ae1f18f2b841bfb80d30500ee2
Cătălin Jitea <catalinjitea@gmail.com>
2019-07-15 18:01:02 +0300

    [refs #107764] Add script to complete the new field check_passed

--- cache_registry/manager.py
+++ cache_registry/manager.py
@@ -3,7 +3,8 @@ import pprint
 
 from flask.ext.script import Manager
 
-from cache_registry.models import User
+from cache_registry.models import db, User, Undertaking
+from cache_registry.sync.fgases import eea_double_check_fgases
 
 
 utils_manager = Manager()
@@ -20,3 +21,41 @@ def check_integrity():
          for (e, ) in duplicates}
 
     pprint.pprint(d)
+
+
+@utils_manager.command
+def check_passed():
+    undertakings = Undertaking.query.all()
+    for undertaking in undertakings:
+        highleveluses = []
+        for businessprofile in undertaking.businessprofiles:
+            highleveluses.append(businessprofile.highleveluses)
+        types = []
+        for type in undertaking.types:
+            types.append(type.type)
+        contact_persons = []
+        for contact_person in undertaking.contact_persons:
+            contact_persons.append({
+                "userName": contact_person.username,
+                "firstName": contact_person.first_name,
+                "lastName": contact_person.last_name,
+                "emailAddress": contact_person.email
+            })
+        data = {
+            'id': undertaking.id,
+            'status': undertaking.status,
+            'businessProfile': {
+                'highLevelUses': highleveluses
+            },
+            'types': types,
+            'contactPersons': contact_persons,
+            'address': {
+                'country': {
+                    'type': undertaking.address.country.type
+                }
+            },
+            'euLegalRepresentativeCompany': undertaking.represent,
+            'domain': undertaking.domain
+        }
+        undertaking.check_passed = eea_double_check_fgases(data)
+        db.session.commit()
