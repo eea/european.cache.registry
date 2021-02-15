4e77a8042a0cc7211de0e4a450ab7e38c8235ea9
dianaboiangiu <boiangiudiana58@gmail.com>
2021-02-15 15:34:57 +0200

    Allow GB to report without representative

--- cache_registry/sync/fgases.py
+++ cache_registry/sync/fgases.py
@@ -28,7 +28,7 @@ def eea_double_check_fgases(data):
     country_code = data['address']['country']['code']
     has_eu_legal_rep = data.get('euLegalRepresentativeCompany')
 
-    if all([country_type == 'NONEU_TYPE' or country_type== 'AMBIGUOUS_TYPE' or country_code == 'UK', not has_eu_legal_rep]):
+    if all([country_type == 'NONEU_TYPE', not has_eu_legal_rep]):
         message = 'NONEU_TYPE Companies must have a representative.'
         current_app.logger.warning(message + identifier)
         ok = False
